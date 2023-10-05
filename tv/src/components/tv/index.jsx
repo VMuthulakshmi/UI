a21ceec55828d26d32b082f72fe456c21d0a7d0f
Muthulakshmi <vmuthulakshmi1803@gmail.com>
2023-10-05 20:30:58 +0530

    Commit

---
+++ tv/src/components/tv/index.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+import Image from '../image';
+import data from "../../data.json";
+import PropTypes from 'prop-types';
+
+import "./TV.scss";
+
+function getChannel(channels,index){
+    return (
+        <div>
+            <div key={channels[index].id} className={`each-channel ${channels[index].subscribed? 'subscribed':'not-subscribed'}`}>
+                <div className='channel-no'>{channels[index].channelNo}</div>
+                <Image src={channels[index].image} className={'channel-image'}/>
+            </div> 
+        </div>  
+    )
+}
+
+
+function setSubscribe(channels,index,setChannels){
+    console.log("Here///")
+    setChannels((prevState) => 
+        prevState.map((item) =>
+        {
+            console.log("item ",item.id,index);
+            return (          
+                item.id == (index+1) ? { ...item, subscribed: true } : item
+                )
+        }
+
+        ),
+    );
+//     setChannels(prevState => ({
+//         ...prevState,
+//         prevState[index].subscribed :true,
+//   })
+// )
+
+    // for(let i=0;i<channels.lenght;i++){
+    //     setChannels()
+    // }
+  //  setChannels(prevState=>{prevState})
+    // setChannels(prevState=>({
+    //     channels: prevState.map((ele)=>{
+    //         // console.log("Element ",ele);
+    //         // return ele;
+    //         // if(ele.id=== index)
+    //         // {
+    //         //     return ele.subscribed=true   
+    //         // }else{
+    //         //     return ele;
+    //         // }
+    //     })
+    // }))
+}
+
+function subscriptionLayout(channels,index,setChannels){
+    if(channels[index] && !channels[index].subscribed){
+       return (
+            <div>
+                <div className='subscribed-text'>Channel is not subscribed</div>
+                <div className='not-subscribed' onClick={()=>{setSubscribe(channels,index,setChannels)}}>SUBSCRIBE</div>
+            </div>
+       ) 
+    }
+    return (<div>{channels[index].subscribed }</div>)
+}
+
+function getVolume(volume){
+    const rows = [];
+    for(let i = 0; i < volume; i++) {
+        rows.push( <div key={i} className='white-bar'></div>);
+    }
+   return rows;
+}
+
+
+export default function TV({on,activeChannel,volume}) {
+
+    const [showVol, setShowVol] = useState(false);
+    const [showChNo, setShowChNo] = useState(false);
+    const [channels, setChannels] = useState(data)
+
+
+    useEffect(()=>{
+        console.log("Channels",channels)
+    },[channels])
+    useEffect(() => { 
+        setShowVol(true);
+        // Creating a timeout within the useEffect hook 
+        setTimeout(() => { 
+            setShowVol(false); 
+        }, 5000); 
+    }, [volume]); 
+
+    useEffect(() => { 
+        setShowChNo(true);
+        // Creating a timeout within the useEffect hook 
+        setTimeout(() => { 
+            setShowChNo(false); 
+        }, 3000); 
+    }, [activeChannel]); 
+
+    return (
+        <div className={`tv ${on ? 'show': ''}`}>
+            <div className={`channels  ${showChNo ? '': 'hide'} `}>
+                {getChannel(channels,activeChannel) }
+            </div>
+            
+            <div className={`volume-status ${showVol ? '': 'hide'}`}>
+                <div className='volume-text'>{volume}</div>
+                {
+                    getVolume(volume)
+                }
+            </div>
+
+            <div className='subscription-block'>
+                {subscriptionLayout(channels,activeChannel,setChannels)}
+            </div>
+        </div>
+    )
+}
+
+
+TV.propTypes = {
+    on: PropTypes.bool,
+    activeChannel: PropTypes.number,
+    volume: PropTypes.number
+};
---
+++ tv/src/pages/Home/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import TV from '../../components/tv';
+import channels from "../../data.json";
+import "./Home.scss";
+
+
+export default function Home() {
+
+    const [on,setOn] = useState(false);
+    const [vol,setVol] = useState(10);
+    const [activeChannel,setActiveChannel] = useState(1);
+    const maxVol = 20
+    const maxChannel = channels.length;
+
+
+    return (
+        
+        <div className={`home tv-${on ? 'on' : ''}`}>
+            <TV on={on} activeChannel={activeChannel} volume={vol}/>
+            <div className='btn-on-off'>
+                <div className= {`btn-block ${on ? 'active' : ''}`} onClick={()=>{setOn(!on)}}>
+                    <div className="each-btn on">ON</div>
+                    <div className="each-btn off">OFF</div>
+                </div>
+            </div>
+            <div className='program-control'>
+                <div className='controls'>
+                    <div className='each-control' onClick={()=>setVol((vol+1)<=maxVol ? vol+1 :vol )}>+</div>
+                    <div className='each-control' onClick={()=>setVol((vol-1)>=0 ? vol-1 :vol )}>-</div>
+                </div>
+            </div>
+            <div className='volume-control'>
+                <div className='controls'>
+                    <div className='each-control' onClick={()=>setActiveChannel((activeChannel+1) % maxChannel)}>+</div>
+                    <div className='each-control' onClick={()=>setActiveChannel((activeChannel-1) % maxChannel)}>-</div>
+                </div>
+            </div>
+        </div>
+    )
+}
